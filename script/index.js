c1fde5ee93be0010f04e636ff952aab6668793e7
pavlikpaavlik <pavlikpavlik.m@yandex.ru>
2020-07-07 17:50:52 +0300

    fix: js, html

--- script/index.js
+++ script/index.js
@@ -1,27 +1,33 @@
-const openPopupButton = document.querySelector('.profile-info__edit-button');
+const openPopupButton = document.querySelector('.profile__edit-button');
 const closePopupButton = document.querySelector('.popup__close-button');
 const popup = document.querySelector('.popup');
 const form = popup.querySelector('.popup__form');
 const formSubmit = form.querySelector('.popup__save');
-let profileName = document.querySelector('.profile-info__title');
-let profileJob = document.querySelector('.profile-info__job');
+let profileName = document.querySelector('.profile__title');
+let profileJob = document.querySelector('.profile__job');
+let inputName = form.querySelector('.popup__input_value_name');
+let inputJob = form.querySelector('.popup__input_value_job');
 
 function togglePopup() {
-    popup.classList.toggle('popup_opened');
-
+    if (popup.classList.contains('popup_opened')) {
+        inputName.value = profileName.textContent;    
+        inputJob.value = profileJob.textContent;
+    }
+    popup.classList.toggle('popup_opened'); 
 }
-
-openPopupButton.addEventListener('click', togglePopup);
-closePopupButton.addEventListener('click', togglePopup);
 
 function formSubmitHandler (event) {
     event.preventDefault();
 
-    let inputName = form.querySelector('.popup__input_name');
-    let inputJob = form.querySelector('.popup__input_job');
+    let editedName = inputName.value;
+    let editedJob = inputJob.value;
 
-    profileName.textContent = inputName.value;
-    profileJob.textContent = inputJob.value;
+    profileName.textContent = editedName;
+    profileJob.textContent = editedJob;
+
+    togglePopup();
 }
 
-formSubmit.addEventListener('click', formSubmitHandler);
+openPopupButton.addEventListener('click', togglePopup);
+closePopupButton.addEventListener('click', togglePopup);
+form.addEventListener('submit', formSubmitHandler);
